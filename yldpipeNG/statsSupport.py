4b66179bc416c55177b2aea91d24f0da910d4db8
groovehunter <groovehunter@posteo.org>
2025-11-03 11:37:03 +0100

    start adapt of old statsSupport

--- yldpipeNG/statsSupport.py
+++ yldpipeNG/statsSupport.py
@@ -4,17 +4,23 @@ logger = setup_logger(__name__, __name__+'.log')
 
 
 class StatsSupport:
+    """
     df_d = {}
     df_d['stats'] = {}
     buffer_names_d = {}
     frame_fields = {}
     results = {}
-
+    """
+    # backward compatibility
     def stats_df_init(self):
+        self.stats_init_df()
+
+    def stats_init_df(self):
         self.df_d['stats']['report'] = pd.DataFrame(columns=self.frame_fields['stats_table'])
         self.buffer_names_d['stats']['report'] = 'report'
 
     def stats_init(self):
+        logger.debug("Initializing stats")
         self.count = 0
         self.count_suc = 0
         self.count_err = 0
@@ -30,6 +36,8 @@ class StatsSupport:
         logger.info("self.count_suc: %s", self.count_suc)
         logger.info("self.count_crit: %s", self.count_crit)
         logger.info("self.count_err: %s", self.count_err)
+
+    def stats_report_df(self, name=''):
         ldf = len(self.df_d['stats']['report'])
         logger.debug('len(self.df_d[stats][report]): %s', ldf)
         self.df_d['stats']['report'].loc[ldf] = {
