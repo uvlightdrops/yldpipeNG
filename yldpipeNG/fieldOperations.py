758afec620e5a4b400f9ee4f63057a3f511e69a1
groovehunter <groovehunter@posteo.org>
2025-11-17 22:11:23 +0100

    v0.4 pl stable, check terms in rows

--- yldpipeNG/fieldOperations.py
+++ yldpipeNG/fieldOperations.py
@@ -4,30 +4,31 @@ logger = setup_logger(__name__, __name__+'.log')
 
 import re
 
-class FieldOps:
-    """ FieldOps - Field Operations
-        This class is used to perform operations on fields of the data
-        that is processed by the pipeline.
-    """
 
+# this class should be similar to FieldOps, but it works on rows directly
+# we do not use any entry object here anymore
 
-    # use dict or some standard for entry  XXX
-    def check_entry_for_prominent_terms(self, entry, row):
-        """ in entries there are certain terms at typical places,
+class FieldOperations:
+
+    def check_row_for_prominent_terms(self, row):
+        """ in rows there are certain terms at typical places,
             like in the title or the notes, ie. the behoerde or the criticality
         """
         self.config_d = self.context['config_d']
-        # XXX make this much more specific, which fields should be searched in for a value
         field_search_spec   = self.config_d['kp_term_attr_field_search_spec']['field_search_spec']
-        # instead of search all everywhere, this is an extra option as last resort
         field_variations    = self.config_d['kp_term_attr_field_variations']['field_variations']
         field_search        = self.config_d['kp_term_attr_field_search']['field_search']
+
         kp_process_fields = self.config_d['kp_process_fields']
         attrs_all = kp_process_fields['kp_pure_fields'] + \
                     kp_process_fields['kp_same_fields']
 
         if 'password' in attrs_all:
             attrs_all.remove('password')
+        status_d = {
+            'status': None,
+            'status_info': '',
+        }
         age_sig = {
             'hostname': None,
             'item': None,
@@ -36,27 +37,29 @@ class FieldOps:
             'behoerde': None,
             'url': None,
         }
-        # logger.debug('group: %s - entry: %s', 'see log-2', entry)
         ### here submethods
-        self.do_search_spec(field_search_spec, entry, age_sig)
-        self.do_variations(field_variations, entry, age_sig, attrs_all)
-        self.do_search(field_search, entry, age_sig, attrs_all, row)
+        self.do_search_spec(field_search_spec, row, age_sig)
+        self.do_variations(field_variations, row, age_sig, attrs_all)
+        self.do_search(field_search, age_sig, status_d, attrs_all, row)
 
-        logger.debug('age_sig: %s', age_sig)
+        #logger.debug('age_sig: %s', age_sig)
         result = {}
         # prepare transfer
         for key, d_item in age_sig.items():
             # what condition was here? XXX
             if True:
                 result['sig_'+key] = d_item
-            #if key == 'item':
-            #    result['item'] = d_item
+        for key, d_item in status_d.items():
+            result[key] = d_item
+
+        #logger.debug('result: %s', result)
         row.update( result )
-        # logger.debug('row: %s', row)
+        #logger.debug('row: %s', row)
         return row  #age_sig
 
 
-    def do_search_spec(self, field_search_spec, entry, age_sig):
+
+    def do_search_spec(self, field_search_spec, row, age_sig):
             for item in field_search_spec:
                 # logger.debug('item: %s', item['name'])
                 method, argu = next(iter(item['how'].items()))
@@ -67,7 +70,7 @@ class FieldOps:
                     # if there are more than one, we log a warning
                     # if there is only one, we keep it
                     where = item['where']
-                    val = getattr(entry, where)
+                    val = row[where]
                     if val is None:
                         # logger.debug('val is None')
                         val = ''
@@ -77,10 +80,10 @@ class FieldOps:
                             logger.debug('SS val: %s, m: %s', val, m)
                             age_sig[item['finds']] = m[0]
 
-    def do_variations(self, field_variations, entry, age_sig, attrs_all):
+    def do_variations(self, field_variations, row, age_sig, attrs_all):
             for attr in attrs_all:
-                text = str(getattr(entry, attr))
-                # logger.debug('attr: %s - text: %s', attr, text)
+                text = str(row[attr])
+                logger.debug('attr: %s - text: %s', attr, text)
                 if text is None or text=='':
                     continue
 
@@ -110,38 +113,40 @@ class FieldOps:
         # Search all values for these strings
         #for fs_key, fs_item in field_search.items():
         # logger.debug('fs_key: %s, fs_item: %s', fs_key, fs_item)
-    def do_search(self, field_search, entry, age_sig, attrs_all, row):
-            for attr in attrs_all:
-                text = str(getattr(entry, attr))
+    def do_search(self, field_search, age_sig, status_d, attrs_all, row):
+        for attr in attrs_all:
+            text = str(row[attr])
 
-                for fs_item in field_search:
-                    name = fs_item['name']
+            for fs_item in field_search:
+                name = fs_item['name']
 
-                    if fs_item['read'] == 're':
-                        m = None
-                        cpat = re.compile(fs_item['args'])
-                        m = cpat.findall(text)
+                if fs_item['read'] == 're':
+                    m = None
+                    cpat = re.compile(fs_item['args'])
+                    m = cpat.findall(text)
+                    if m:
+                        age_sig[name] = m[0]
+                        # logger.debug('fs_key: %s - text: %s, m: %s', fs_key, text, m)
+
+                if fs_item['read'] == 'enum':
+                    act = fs_item['act']
+                    for term in fs_item['args']:
+                        m = re.findall(term, text)
                         if m:
-                            age_sig[name] = m[0]
-                            # logger.debug('fs_key: %s - text: %s, m: %s', fs_key, text, m)
-
-                    if fs_item['read'] == 'enum':
-                        act = fs_item['act']
-                        for term in fs_item['args']:
-                            m = re.findall(term, text)
-                            if m:
-                                logger.debug('DS text: %s, m: %s', text, m)
-                                if 'set_sig' in act.keys():
-                                    age_sig[act['set_sig']] = m[0]
-                                if 'mark'  in act.keys():
-                                    # logger.debug('== mark: %s', act['mark'])
-                                    row['status'] = act['mark']
-                                    row['status_info'] = row['status_info'] + ' ' + m[0][:8]
-                                if 'modify' in act.keys():
-                                    field = act['modify']['field']
-                                    val =  act['modify']['val']
-                                    row[field] = val
-                                # one match is sufficient
-                                continue
-                                # XXX modifying row here is a no-go!
+                            logger.debug('DS text: %s, m: %s', text, m)
+                            if 'set_sig' in act.keys():
+                                age_sig[act['set_sig']] = m[0]
+                            if 'mark'  in act.keys():
+                                # logger.debug('== mark: %s', act['mark'])
+                                status_d['status'] = act['mark']
+                                status_d['status_info'] = status_d['status_info'] + ' ' + m[0][:8]
+                            """
+                            if 'modify' in act.keys():
+                                field = act['modify']['field']
+                                val =  act['modify']['val']
+                                age_sig[field] = val
+                            """
+                            # one match is sufficient
+                            continue
+                            # XXX modifying row here is a no-go!
 
